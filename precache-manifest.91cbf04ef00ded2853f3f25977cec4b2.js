7068fa2ee351e4e53f520e7c27c0e959b4b3f2c0
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-07-18 16:17:04 +0530

    Updates

---
+++ precache-manifest.91cbf04ef00ded2853f3f25977cec4b2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d7371d36e47acb18514d60890dd89e",
+    "url": "/deepanshu2506.github.io/index.html"
+  },
+  {
+    "revision": "16df859524343bcfa6b5",
+    "url": "/deepanshu2506.github.io/static/css/3.48a93ee6.chunk.css"
+  },
+  {
+    "revision": "ca219c626c380f115c83",
+    "url": "/deepanshu2506.github.io/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "fdc1d821100d91696a25",
+    "url": "/deepanshu2506.github.io/static/css/main.1b41f436.chunk.css"
+  },
+  {
+    "revision": "b346a377dd0ee65dd7b8",
+    "url": "/deepanshu2506.github.io/static/js/2.c91572f3.chunk.js"
+  },
+  {
+    "revision": "16df859524343bcfa6b5",
+    "url": "/deepanshu2506.github.io/static/js/3.279cb3b2.chunk.js"
+  },
+  {
+    "revision": "ca219c626c380f115c83",
+    "url": "/deepanshu2506.github.io/static/js/4.f5af30b9.chunk.js"
+  },
+  {
+    "revision": "fdc1d821100d91696a25",
+    "url": "/deepanshu2506.github.io/static/js/main.a5add595.chunk.js"
+  },
+  {
+    "revision": "860cd1d48eeaf773a3d5",
+    "url": "/deepanshu2506.github.io/static/js/runtime-main.b5cb2bab.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/deepanshu2506.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/deepanshu2506.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "bfc2772fa4b86f70329793ba21f39da7",
+    "url": "/deepanshu2506.github.io/static/media/billme.bfc2772f.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/deepanshu2506.github.io/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/deepanshu2506.github.io/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/deepanshu2506.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/deepanshu2506.github.io/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "b26709034c390a0261debf795cbf10ea",
+    "url": "/deepanshu2506.github.io/static/media/kjsce.b2670903.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deepanshu2506.github.io/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/deepanshu2506.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/deepanshu2506.github.io/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/deepanshu2506.github.io/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/deepanshu2506.github.io/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/deepanshu2506.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/deepanshu2506.github.io/static/media/talksCardBack.de3da806.svg"
+  }
+]);
